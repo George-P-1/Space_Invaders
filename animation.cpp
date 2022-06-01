20b6d3bd8e28f57f46e02e14404837837427dde8
George <100698568+George-P-1@users.noreply.github.com>
2022-06-01 23:30:10 +0200

    Implemented Polymorphism and Simplified Code
    Added new type of enemy.
    Created a base Enemy Class.
    Created Destroyer and Ship Class that inherits from Enemy Class.
    Created Setting Class.
    Created Animation Class.

---
+++ animation.cpp
@@ -0,0 +1,106 @@
+#include "animation.h"
+
+#include <iostream>
+
+Animation::Animation(sf::Texture &texture, int t)
+{
+    this->setTexture(texture);
+    this->setTextureRect(sf::IntRect(0, 0, spritedim, spritedim));
+    this->type  = t;
+    if(this->type == 1) setShipParameters();
+    else if(this->type == 2) setDestroyerParameters();
+}
+
+void Animation::playAnimation(sf::Time &elapsed)
+{
+    if(this->type == 1) this->playShipAnimation(elapsed);
+    else if(this->type == 2) this->playDestroyerAnimation(elapsed);
+}
+
+//// For old explosion sprite
+//void Animation::setShipParameters()
+//{
+//    this->setScale((float)1/3, (float)1/3); // 192 / 3 = 64
+//    spritedim = 192; // Dimension of one sprite in sprite sheet. 192x192
+//    switch_time = 0.05; // 20 fps. Will play all 10 frames of sprite sheet in 0.5 second.
+//    totalrow = 2;
+//    totalcolumn = 5;
+//    currentrow = 1;
+//    currentcolumn = 1;
+//}
+
+//void Animation::playShipAnimation(sf::Time &elapsed)
+//{
+//    // Set animation position
+//    this->setPosition(this->enemyposition.x, this->enemyposition.y); // Center animation wrt to enemy sprite
+
+//    this->total_time += elapsed.asSeconds();
+
+//    if(this->total_time >= this->switch_time)
+//    {
+//        this->total_time -= this->switch_time; // Almost same as total_time = 0;. In the long run this will prevent errors caused by approximations.
+//        this->setTextureRect(sf::IntRect((this->currentcolumn-1) * this->spritedim, (this->currentrow-1) * this->spritedim, this->spritedim, this->spritedim));
+//        this->currentcolumn++; // Go to next column
+//        if(this->currentcolumn == this->totalcolumn && this->currentrow != this->totalrow) // After animating first row of sprite sheet
+//        {
+//            this->currentrow++;
+//            this->currentcolumn = 1;
+//        }
+//        if(this->currentrow >= this->totalrow && this->currentcolumn >= this->totalcolumn) // If one animation is done
+//        {
+//            this->explosion = false;
+//            this->currentcolumn = 1;
+//            this->currentrow = 1;
+//        }
+//    }
+//}
+
+// For new animation spritesheet
+void Animation::setShipParameters()
+{
+    this->setScale((float)1/2, (float)1/2); // 256 / 4 = 64 // 256 / 2 = 128
+    spritedim = 256; // Dimension of one sprite in sprite sheet. 256x256
+    switch_time = 0.02; // 0.02 -> 50 fps
+    totalrow = 1;
+    totalcolumn = 64;
+    currentrow = 1;
+    currentcolumn = 1;
+}
+
+void Animation::playShipAnimation(sf::Time &elapsed)
+{
+    // Same animation code for new explosion spritesheet
+    this->playDestroyerAnimation(elapsed);
+}
+
+void Animation::setDestroyerParameters()
+{
+//    this->setScale((float)1/1.6, (float)1/1.6); // 192 / 1.6 = 120
+    this->setScale((float)1/1.2, (float)1/1.2); // 192 / 1.2 = 160
+    spritedim = 192; // 192x192
+    switch_time = 0.02; // 0.02 -> 50 fps
+    totalrow = 1;
+    totalcolumn = 64;
+    currentrow = 1;
+    currentcolumn = 1;
+}
+
+void Animation::playDestroyerAnimation(sf::Time &elapsed)
+{
+    // Set animation position
+    this->setPosition(this->enemyposition.x, this->enemyposition.y); // Center animation wrt to enemy sprite
+
+    this->total_time += elapsed.asSeconds();
+
+    if(this->total_time >= this->switch_time)
+    {
+        this->total_time -= this->switch_time; // Almost same as total_time = 0;. In the long run this will prevent errors caused by approximations.
+        this->setTextureRect(sf::IntRect((this->currentcolumn-1) * this->spritedim, 0, this->spritedim, this->spritedim));
+        this->currentcolumn++; // Go to next column
+        if(this->currentcolumn >= this->totalcolumn) // If one animation is done
+        {
+            this->explosion = false;
+            this->currentcolumn = 1;
+        }
+    }
+}
